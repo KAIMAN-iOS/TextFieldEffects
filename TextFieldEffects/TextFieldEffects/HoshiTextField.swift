1d35118960037a8b777a11f388b7ff976835fec7
jerometonnelier <jerometonnelier@yahoo.fr>
2021-04-09 17:20:21 +0200

    public

--- TextFieldEffects/TextFieldEffects/HoshiTextField.swift
+++ TextFieldEffects/TextFieldEffects/HoshiTextField.swift
@@ -69,9 +69,9 @@ import UIKit
         }
     }
     
-    private let borderThickness: (active: CGFloat, inactive: CGFloat) = (active: 2, inactive: 0.5)
-    private let placeholderInsets = CGPoint(x: 0, y: 6)
-    private let textFieldInsets = CGPoint(x: 0, y: 12)
+    public var borderThickness: (active: CGFloat, inactive: CGFloat) = (active: 2, inactive: 0.5)
+    public var placeholderInsets = CGPoint(x: 0, y: 6)
+    public var textFieldInsets = CGPoint(x: 0, y: 12)
     private let inactiveBorderLayer = CALayer()
     private let activeBorderLayer = CALayer()    
     private var activePlaceholderPoint: CGPoint = CGPoint.zero
